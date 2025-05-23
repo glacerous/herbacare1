eade96cb7c64ad93d3d3568e9e02289b9b175457
glacerous <167007103+glacerous@users.noreply.github.com>
2025-05-24 00:22:15 +0700

    Delete herbacare directory

--- herbacare/proses/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start(); 
-    session_destroy(); 
-    echo "<script>
-            alert('Logout Berhasil');
-            window.location.href = '../pages/login.php';
-            </script>";
-?>
